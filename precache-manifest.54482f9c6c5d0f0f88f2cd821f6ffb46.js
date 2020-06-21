00e9485db6c3e37c908b2836f5a8064e5f6726a4
ulug94 <uluginha@gamil.com>
2020-06-21 10:05:02 +0500

    Updates

---
+++ precache-manifest.54482f9c6c5d0f0f88f2cd821f6ffb46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5795f07a56cac380023770c49bf88a",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "5d3226e4c24cc1339081",
+    "url": "/robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3eb727421052542ac81e",
+    "url": "/robots/static/css/main.a1203613.chunk.css"
+  },
+  {
+    "revision": "5d3226e4c24cc1339081",
+    "url": "/robots/static/js/2.cb35dbec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots/static/js/2.cb35dbec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb727421052542ac81e",
+    "url": "/robots/static/js/main.ae22fabe.chunk.js"
+  },
+  {
+    "revision": "bf23a7a901543060abcc",
+    "url": "/robots/static/js/runtime-main.c402b3dc.js"
+  },
+  {
+    "revision": "92c3810f48104a0fa26cbef3ad302807",
+    "url": "/robots/static/media/electrical.92c3810f.woff"
+  }
+]);
